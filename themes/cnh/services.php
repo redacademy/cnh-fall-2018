06049ef3097dfbeab55cccb018e2968f4e5fa1c2
Kyle_Tecson <kyltecs@gmail.com>
2018-12-05 20:56:36 -0800

    add style to service page

--- themes/cnh/services.php
+++ themes/cnh/services.php
@@ -10,12 +10,13 @@ get_header(); ?>
 <div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
+			<div class="center-page">
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'template-parts/content', 'page' ); ?>
 
 			<?php endwhile; // End of the loop. ?>
-
+			</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
