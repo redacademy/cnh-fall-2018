1f59891d036340ee939ee608cb33323a18cd7bd0
Kyle_Tecson <kyltecs@gmail.com>
2018-12-05 23:40:21 -0800

    styled service page and about page for desktop and mobile

--- themes/cnh/services.php
+++ themes/cnh/services.php
@@ -11,11 +11,12 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
 			<div class="center-page">
-			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+				<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php endwhile; // End of the loop. ?>
+					<?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+				<?php endwhile; // End of the loop. ?>	
 			</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
