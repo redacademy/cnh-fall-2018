b5bf1d0dd1b465679ce78402d38e01f1ee38c819
nejmal <lam.jennifer.ky@gmail.com>
2018-12-07 10:48:45 -0800

    Add material icons font.

--- themes/cnh/js/custom.js
+++ themes/cnh/js/custom.js
@@ -2,7 +2,9 @@
   $(function () {
 
     const $sideMenu = $('#side-menu');
+    // const $firstOpt = $('.goog-te-combo option:first-child');
 
+    // start of side menu
     $('.btn-open').on('click', function (event) {
       event.preventDefault();
       openSideMenu();
@@ -20,28 +22,6 @@
     function closeSideMenu() {
       $sideMenu.css('width', '0');
     }
-  // start of spectagram
-  // var spectragramComplete = function () {
-  var spectra = {
-    instaToken : '9448460219.0d61304.de737249603f46ec9356ff60f0aabdcf',
-    instaID: '0d61304f0d4242a9b55e5c1378ba869e',
-  
-
-  init: function () {
-  $.fn.spectragram.accessData = {
-    accessToken: this.instaToken,
-    clientID: this.instaID
-  };
-
-  $('.instalame-container').spectragram('getUserFeed',{
-    // complete : spectragramComplete(),
-    max: 2,
-    size: "medium",
-    wrapEachWith: '<div class="photo">'
-  });
- }
-}
-spectra.init();
 
     $('.side-navbar').find('.accordion-toggle').click(function () {
       $(this).next().toggleClass('open').slideToggle('fast');
@@ -49,7 +29,33 @@ spectra.init();
 
       $('.side-navbar .accordion-content').not($(this).next()).slideUp('fast').removeClass('open');
       $('.side-navbar .accordion-toggle').not(jQuery(this)).removeClass('active-tab').find('.side-link').removeClass('active');
-    });
+    }); // end of side menu
+
+    // start of Google Translate changes
+    $('.goog-te-combo option').css('color', 'red');
+
+    // start of spectagram
+    // var spectragramComplete = function () {
+    var spectra = {
+      instaToken: '9448460219.0d61304.de737249603f46ec9356ff60f0aabdcf',
+      instaID: '0d61304f0d4242a9b55e5c1378ba869e',
+
+
+      init: function () {
+        $.fn.spectragram.accessData = {
+          accessToken: this.instaToken,
+          clientID: this.instaID
+        };
+
+        $('.instalame-container').spectragram('getUserFeed', {
+          // complete : spectragramComplete(),
+          max: 2,
+          size: "medium",
+          wrapEachWith: '<div class="photo">'
+        });
+      }
+    }
+    spectra.init();
 
   }); // end of doc ready
 })(jQuery);
