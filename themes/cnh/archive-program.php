5630a59ce10b3321873c22f9f9661a2e73825cdc
nejmal <lam.jennifer.ky@gmail.com>
2018-12-10 20:29:24 -0800

    Add variables for translator js.

--- themes/cnh/archive-program.php
+++ themes/cnh/archive-program.php
@@ -61,16 +61,3 @@ get_header(); ?>
   </div><!-- #primary -->
   
 <?php get_footer(); ?>
-© 2018 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Help
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
-Press h to open a hovercard with more details.
