743921e269b7f28d72e878dbec4834fe864ce5c1
mike <mike_4596@hotmail.com>
2018-12-14 11:47:13 -0800

    add and style frontpage buttons

--- themes/cnh/front-page.php
+++ themes/cnh/front-page.php
@@ -96,6 +96,9 @@ $event_id = get_the_ID();
 			
 
 				<div class="frontpage-buttons">
+					<a class="childcare-button button-yellow all-button" href="/childcare">childcare</a>
+					<a class="senior-programs-button button-pink all-button" href="/senior-programs">senior programs</a>
+					<a class="donate-button button-yellow all-button" href="/donate">donate</a>
 				</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
