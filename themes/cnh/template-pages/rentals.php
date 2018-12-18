f3bfb1408853d0d9816e92cac7304e19815a2e8a
Kyle_Tecson <kyltecs@gmail.com>
2018-12-17 16:46:01 -0800

    style rentals page

--- themes/cnh/template-pages/rentals.php
+++ themes/cnh/template-pages/rentals.php
@@ -11,47 +11,41 @@ get_header(); ?>
 <div id="primary" class="content-area">
   <main id="main" class="site-main" role="main">
   
-    <div class="rooms-carousel-container">
-        <?php
-        
-        //TODO use if statement to check if field is not empty
-    
-         $rooms = CFS()->get( 'rental_room' ); // get CFS loop for rooms   ?>
+  <div class="rooms-carousel-container">
+        <?php $rooms = CFS()->get( 'rental_room' ); // get CFS loop for rooms  ?>
         <div>
           <ul class="rooms-carousel-list">
             <?php foreach ( $rooms as $room ): ?>
               <li><?php echo $room['room_title']; ?></li>
             <?php endforeach; ?>
           </ul>
-          </div>
+        </div>
+        <!-- Flickity Carousel -->
+        <div class="main-carousel">
           <?php
           foreach ( $rooms as $room ): ?>
-          <div class="room-wrapper">
-              <h2 class="carousel-rental-title"><?php echo $room['room_title']; ?></h2>
-              <div class="main-carousel">            
-                <div class="carousel-cell">
-                  <img src="<?php echo $room['image_loop'][0]['room_image']; ?>" />
-                </div>
-              </div>
+          <div class="room-separate">
+            <div class="carousel-cell">
+              <img src="<?php echo $room['image_loop'][0]['room_image']; ?>" />
+            </div>
+            
             <div class="room-stats">
-                <div class="room-info">
-                      <?php echo $room['room_measurements']; ?>
-                </div>
-                <div class="room-info">
-              <?php echo $room['room_capacity']; ?>
-                </div>
-                <div class="room-info">
-              <?php echo $room['room_features']; ?>
-                </div>
+              <div class="room-info">
+                <?php echo $room['room_measurements']; ?>
+              </div>
+          
+              <div class="room-info">
+                <?php echo $room['room_capacity']; ?>
+              </div>
+            
+              <div class="room-info">
+                <?php echo $room['room_features']; ?>
+              </div>
             </div>
           </div>
-          <?php endforeach; ?>
+          <?php endforeach ?>
         </div><!-- / Flickity Carousel -->
-  
-
-
-
-
+      </div><!-- / rooms-carousel-container -->
 
     <?php while ( have_posts() ) : the_post(); ?>
 
