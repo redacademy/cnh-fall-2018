7d2a104e0f18ec015440f9d24dcdb32c2979646a
mike <mike_4596@hotmail.com>
2019-02-21 14:49:25 -0800

    test

--- themes/cnh/header.php
+++ themes/cnh/header.php
@@ -13,6 +13,10 @@
 		<link rel="profile" href="http://gmpg.org/xfn/11">
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
     <link rel="stylesheet" type="text/css" href="mystyle.css">
+    <link rel="stylesheet" type="text/css" href="../mystyle.css">
+    <link rel="stylesheet" type="text/css" href="./mystyle.css">
+    <link rel="stylesheet" type="text/css" href="/mystyle.css">
+    <link rel="stylesheet" type="text/css" href="//mystyle.css">
 
 	<?php wp_head(); ?>
 	</head>
