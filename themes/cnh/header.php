62aa7a64be7d54731d912058188cf345b08c6de7
mike <mike_4596@hotmail.com>
2019-02-21 15:41:28 -0800

    test fix

--- themes/cnh/header.php
+++ themes/cnh/header.php
@@ -12,7 +12,6 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="profile" href="http://gmpg.org/xfn/11">
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-    <link rel="stylesheet" type="text/css" href="mystyle.css">
 
 	<?php wp_head(); ?>
 	</head>
