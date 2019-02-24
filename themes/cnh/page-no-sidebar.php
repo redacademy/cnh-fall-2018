3f706b64f71679fde7191a763c26b0478ff8b67e
mike <mike_4596@hotmail.com>
2019-02-24 11:41:01 -0800

    test

--- themes/cnh/page-no-sidebar.php
+++ themes/cnh/page-no-sidebar.php
@@ -15,7 +15,7 @@ get_header(); ?>
 </header><!-- .page-header -->
 
 <div id="primary" class="content-area">
-  <main id="main" class="site-main" role="main">test
+  <main id="main" class="site-main" role="main">
 
     <?php while ( have_posts() ) : the_post(); ?>
 
