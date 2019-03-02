e62af559bed74f063181a804cc4b108581f5eaef
mike <mike_4596@hotmail.com>
2019-03-02 11:31:58 -0800

    readmore test

--- themes/cnh/search.php
+++ themes/cnh/search.php
@@ -22,9 +22,9 @@ get_header(); ?>
 
         <?php get_template_part( 'template-parts/content', 'search' ); ?>
         
-      <button>
+
 			<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-      </button>
+
 
 			<?php endwhile; ?>
 
