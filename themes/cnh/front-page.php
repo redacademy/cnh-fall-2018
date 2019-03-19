b41499c55ca835e30d9eafe7bcaa9d9079706a2c
mike <mike_4596@hotmail.com>
2019-03-18 19:46:35 -0700

    insta

--- themes/cnh/front-page.php
+++ themes/cnh/front-page.php
@@ -79,7 +79,7 @@ $event_id = get_the_ID();
 	<h2 class="instagram-updates-title"> Instagram Updates</h2>
 				<div class="instagram-container">
 				<script type="text/javascript">
-    var feed = new Instafeed({
+    var feed = new instafeed({
         get: 'user',
         clientId: 'bd87aad432154b9187ebd906a3a7dbdd'
     });
