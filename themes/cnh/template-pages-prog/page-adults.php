d4ce71f514676a38244b473b8923098b0a87ae60
mike <mike_4596@hotmail.com>
2019-04-04 19:57:14 -0700

    add adults page

---
+++ themes/cnh/template-pages-prog/page-adults.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The template for displaying all single services.
+ *
+ * @package based on RED_Starter_Theme
+ * Template Name: adults program
+ */
+get_header(); ?>
+
+<?php get_template_part( 'template-parts/breadcrumbs', 'breadcrumbs' ); ?>
+
+<div id="primary" class="content-area">
+  <main id="main" class="site-main" role="main">
+    
+    <?php get_template_part( 'template-parts/service', 'type' ); ?>
+
+  </main><!-- #main -->
+</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
